dbab5630aa2e748e62b7d8fbe69fdedc69efcd2c
Zoe <ddu_buk_e@naver.com>
2025-04-24 06:31:17 +0900

    fe/init: frontend pjt settings #in-progress

--- Frontend/src/pages/Home.tsx
+++ Frontend/src/pages/Home.tsx
@@ -1,19 +1,5 @@
-interface Test {
-  name: string;
-}
-
-type A = string;
-
 const Home = () => {
-  const test: Test = { name: '11111' };
-  const firstcase: A = 'aaa';
 
-  return (
-    <>
-      <div>안녕</div>
-      <p>{test.name}</p>
-      <p>{firstcase}</p>
-    </>
-  );
-};
-export default Home;
+return <div className="text-green-500">홈이야</div>
+}
+export default Home
